b84b048f247087cf777008a0e63b878923f9ff58
Nazmul Hasan RIVEN <riven25041995@gmail.com>
2023-07-23 11:29:44 +0600

    played a round and console it

--- Rock Paper Scissors/app.js
+++ Rock Paper Scissors/app.js
@@ -1,1 +1,40 @@
-console.error("hi")
+const options = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice (){
+    const choise = options[Math.floor(Math.random() * options.length)];
+    return choise;
+}
+
+let computerSelection = getComputerChoice();
+
+function CheckWinner(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection){
+        return "Tie"
+    }
+    else if (
+        (playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')
+    ){
+        return "Player"
+    }
+    else {
+        return "computer"
+    }
+}
+
+function PlayRound(playerSelection, computerSelection){
+    const result = CheckWinner(playerSelection, computerSelection);
+    if (result == 'Tie'){
+        return `It's a Tie`
+    }
+    else if (result == 'Player'){
+        return `You win! ${playerSelection} beats ${computerSelection}.`
+    }
+    else {
+        return `Computer wins! ${computerSelection} beats ${playerSelection}.`
+    }
+}
+
+const playerSelection = prompt("Choose:" ).toLowerCase();
+console.log(PlayRound(playerSelection, computerSelection))
